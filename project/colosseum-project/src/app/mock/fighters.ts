c7b915f146bfe9e35a2537fd795109b10a7884b4
Crisiiiii <cristiangp9@hotmail.com>
2018-08-10 17:06:06 +0200

    feature(service): add new fighters service

---
+++ project/colosseum-project/src/app/mock/fighters.ts
@@ -0,0 +1,46 @@
+interface Fighters {
+  id: number;
+  name: string;
+  surname: string;
+  date: string;
+  wins: number;
+  lost: number;
+}
+
+export const FIGHTERS: Fighters[] = [
+  {id: 1, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 32, lost: 11},
+  {id: 2, name: 'Arbelas', surname: 'Lucianus Januarius', date: '2016-06-03T22:59:41.518Z', wins: 14, lost: 2},
+  {id: 3, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 32, lost: 11},
+  {id: 4, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 32, lost: 11},
+  {id: 5, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 32, lost: 11},
+  {id: 6, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 32, lost: 11},
+  {id: 7, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 32, lost: 11},
+  {id: 8, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 32, lost: 11},
+  {id: 9, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 32, lost: 11},
+  {id: 10, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 32, lost: 11},
+  {id: 11, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 32, lost: 11},
+  {id: 12, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 32, lost: 11},
+];
+
+ /*
+  post(['2017-08-19T10:05:09.481Z', 'Zosimus Theophilos',   'Andabata',    32, 11]);
+  post(['2016-06-03T22:59:41.518Z', 'Lucianus Januarius',   'Arbelas',     14,  2]);
+  post(['2015-10-16T22:46:41.554Z', 'Servius Glaucia',      'Bestiarius',  69, 13]);
+  post(['2015-07-27T07:14:33.687Z', 'Blandinus Ianuarius',  'Bustuarius',   9,  9]);
+  post(['2016-10-24T22:17:03.687Z', 'Vibius Maximinus',     'Cestus',      56, 32]);
+  post(['2017-01-16T20:01:31.258Z', 'Paulus Traianus',      'Crupellarii', 49, 23]);
+  post(['2016-04-07T10:02:43.622Z', 'Faustinus Herminius',  'Dimachaerus', 27, 18]);
+  post(['2015-08-06T16:44:27.432Z', 'Crispinus Avilius',    'Equites',     19, 12]);
+  post(['2015-04-02T21:11:26.125Z', 'Antoninus Caius',      'Essedarius',  22,  5]);
+  post(['2015-07-22T23:18:29.474Z', 'Aurelius Quintus',     'Retiarius',   63,  0]);
+  post(['2016-03-09T01:34:25.695Z', 'Nonus Severianus',     'Murmillo',    41, 15]);
+  post(['2014-11-05T09:46:10.528Z', 'Septimius Longinus',   'Secutor',     39,  8]);
+  post(['2016-01-03T01:39:00.731Z', 'Blasius Cassius',      'Thraex',      54, 24]);
+  post(['2017-02-02T01:26:05.832Z', 'Septimus Marius',      'Velites',     61, 19]);
+  post(['2017-03-27T17:19:04.605Z', 'Antonius Quintinus',   'Hoplomachus', 33, 28]);
+  post(['2017-06-20T17:07:24.680Z', 'Atilius Tullius',      'Gladiatrix',   2,  1]);
+  post(['2016-11-12T19:42:50.305Z', 'Priscus Julius',       'Bestiarius',  18,  5]);
+  post(['2016-04-17T05:51:46.446Z', 'Aurelianus Iuvenalis', 'Scutarius',   22, 14]);
+  post(['2016-09-30T17:23:36.590Z', 'Iovita Cyprianus',     'Retiarius',   43, 11]);
+  post(['2017-01-31T22:18:19.993Z', 'Brutus Manius',        'Provocator',  16,  8]);
+  post(['2017-02-15T07:27:43.262Z', 'Fulvius Junius',       'Equites',     78, 11]);*/
---
+++ project/colosseum-project/src/app/services/fighters.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ServiceNameService {
+  constructor(private httpClient: HttpClient) { }
+
+}
