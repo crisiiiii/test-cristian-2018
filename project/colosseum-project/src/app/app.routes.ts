3792ae21d1f6ee52425b1cdfb9bdf645a3676e03
Crisiiiii <cristiangp9@hotmail.com>
2018-08-18 13:33:36 +0200

    feature(routes): add routes to components

--- project/colosseum-project/src/app/app.routes.ts
+++ project/colosseum-project/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ChampionsListComponent } from './modules/champions-list/champions-list.component'
+import { ChampionsListComponent } from './modules/champions-list/champions-list.component';
+import { EditFightersComponent } from './modules/edit-fighters/edit-fighters.component';
+import { AddFightersComponent } from './modules/add-fighters/add-fighters.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'champions-list', pathMatch: 'full' },
   { path: 'champions-list', component: ChampionsListComponent },
-  // { path: 'edit-fighters', component: EditFighters }
+  { path: 'edit-fighters', component: EditFightersComponent },
+  { path: 'add-fighters', component: AddFightersComponent },
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
