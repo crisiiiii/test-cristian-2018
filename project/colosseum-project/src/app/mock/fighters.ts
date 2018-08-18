cb76b3fbde8a143b6302b99be68c08af711c16e3
Crisiiiii <cristiangp9@hotmail.com>
2018-08-18 12:03:41 +0200

    refactor(fighters): add fighters

--- project/colosseum-project/src/app/mock/fighters.ts
+++ project/colosseum-project/src/app/mock/fighters.ts
@@ -3,37 +3,23 @@ import { Fighters } from '../interfaces/fighter.interface';
 export const FIGHTERS: Fighters[] = [
   {id: 1, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 32, lost: 11},
   {id: 2, name: 'Arbelas', surname: 'Lucianus Januarius', date: '2016-06-03T22:59:41.518Z', wins: 14, lost: 2},
-  {id: 3, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 5, lost: 10},
-  {id: 4, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 20, lost: 1},
-  {id: 5, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 2, lost: 1},
-  {id: 6, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 5, lost: 8},
-  {id: 7, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 3, lost: 1},
-  {id: 8, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 7, lost: 10},
-  {id: 9, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 21, lost: 15},
-  {id: 10, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 12, lost: 12},
-  {id: 11, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 13, lost: 11},
-  {id: 12, name: 'Andabata', surname: 'Zosimus Theophilos', date: '2017-08-19T10:05:09.481Z', wins: 25, lost: 13},
+  {id: 3, name: 'Bestiarius', surname: 'Servius Glaucia', date: '2015-10-16T22:46:41.554Z', wins: 69, lost: 13},
+  {id: 4, name: 'Bustuarius', surname: 'Blandinus Ianuarius', date: '2015-07-27T07:14:33.687Z', wins: 9, lost: 9},
+  {id: 5, name: 'Cestus', surname: 'Vibius Maximinus', date: '2016-10-24T22:17:03.687Z', wins: 56, lost: 32},
+  {id: 6, name: 'Crupellarii', surname: 'Paulus Traianus', date: '2017-01-16T20:01:31.258Z', wins: 49, lost: 23},
+  {id: 7, name: 'Dimachaerus', surname: 'Faustinus Herminius', date: '2016-04-07T10:02:43.622Z', wins: 27, lost: 18},
+  {id: 8, name: 'Equites', surname: 'Crispinus Avilius', date: '2015-08-06T16:44:27.432Z', wins: 19, lost: 12},
+  {id: 9, name: 'Essedarius', surname: 'Antoninus Caius', date: '2015-04-02T21:11:26.125Z', wins: 22, lost: 5},
+  {id: 10, name: 'Retiarius', surname: 'Aurelius Quintus', date: '2015-07-22T23:18:29.474Z', wins: 63, lost: 0},
+  {id: 11, name: 'Murmillo', surname: 'Nonus Severianus', date: '2016-03-09T01:34:25.695Z', wins: 41, lost: 15},
+  {id: 12, name: 'Secutor', surname: 'Septimius Longinus', date: '2014-11-05T09:46:10.528Z', wins: 39, lost: 8},
+  {id: 13, name: 'Thraex', surname: 'Blasius Cassius', date: '2016-01-03T01:39:00.731Z', wins: 54, lost: 24},
+  {id: 14, name: 'Velites', surname: 'Septimus Marius', date: '2017-02-02T01:26:05.832Z', wins: 61, lost: 19},
+  {id: 15, name: 'Hoplomachus', surname: 'Antonius Quintinus', date: '2017-03-27T17:19:04.605Z', wins: 33, lost: 28},
+  {id: 16, name: 'Gladiatrix', surname: 'Atilius Tullius', date: '2017-06-20T17:07:24.680Z', wins: 2, lost: 1},
+  {id: 17, name: 'Bestiarius', surname: 'Priscus Julius', date: '2016-11-12T19:42:50.305Z', wins: 18, lost: 5},
+  {id: 18, name: 'Scutarius', surname: 'Aurelianus Iuvenalis', date: '2016-04-17T05:51:46.446Z', wins: 22, lost: 14},
+  {id: 19, name: 'Retiarius', surname: 'Iovita Cyprianus', date: '2016-09-30T17:23:36.590Z', wins: 43, lost: 11},
+  {id: 20, name: 'Provocator', surname: 'Brutus Manius', date: '2017-01-31T22:18:19.993Z', wins: 16, lost: 8},
+  {id: 21, name: 'Equites', surname: 'Fulvius Junius', date: '2017-02-15T07:27:43.262Z', wins: 78, lost: 11},
 ];
-
- /*
-  post(['2017-08-19T10:05:09.481Z', 'Zosimus Theophilos',   'Andabata',    32, 11]);
-  post(['2016-06-03T22:59:41.518Z', 'Lucianus Januarius',   'Arbelas',     14,  2]);
-  post(['2015-10-16T22:46:41.554Z', 'Servius Glaucia',      'Bestiarius',  69, 13]);
-  post(['2015-07-27T07:14:33.687Z', 'Blandinus Ianuarius',  'Bustuarius',   9,  9]);
-  post(['2016-10-24T22:17:03.687Z', 'Vibius Maximinus',     'Cestus',      56, 32]);
-  post(['2017-01-16T20:01:31.258Z', 'Paulus Traianus',      'Crupellarii', 49, 23]);
-  post(['2016-04-07T10:02:43.622Z', 'Faustinus Herminius',  'Dimachaerus', 27, 18]);
-  post(['2015-08-06T16:44:27.432Z', 'Crispinus Avilius',    'Equites',     19, 12]);
-  post(['2015-04-02T21:11:26.125Z', 'Antoninus Caius',      'Essedarius',  22,  5]);
-  post(['2015-07-22T23:18:29.474Z', 'Aurelius Quintus',     'Retiarius',   63,  0]);
-  post(['2016-03-09T01:34:25.695Z', 'Nonus Severianus',     'Murmillo',    41, 15]);
-  post(['2014-11-05T09:46:10.528Z', 'Septimius Longinus',   'Secutor',     39,  8]);
-  post(['2016-01-03T01:39:00.731Z', 'Blasius Cassius',      'Thraex',      54, 24]);
-  post(['2017-02-02T01:26:05.832Z', 'Septimus Marius',      'Velites',     61, 19]);
-  post(['2017-03-27T17:19:04.605Z', 'Antonius Quintinus',   'Hoplomachus', 33, 28]);
-  post(['2017-06-20T17:07:24.680Z', 'Atilius Tullius',      'Gladiatrix',   2,  1]);
-  post(['2016-11-12T19:42:50.305Z', 'Priscus Julius',       'Bestiarius',  18,  5]);
-  post(['2016-04-17T05:51:46.446Z', 'Aurelianus Iuvenalis', 'Scutarius',   22, 14]);
-  post(['2016-09-30T17:23:36.590Z', 'Iovita Cyprianus',     'Retiarius',   43, 11]);
-  post(['2017-01-31T22:18:19.993Z', 'Brutus Manius',        'Provocator',  16,  8]);
-  post(['2017-02-15T07:27:43.262Z', 'Fulvius Junius',       'Equites',     78, 11]);*/
